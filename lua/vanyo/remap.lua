bcb07445e2c90c11e6cc878ea7b6e019655149cf
Todd Vanyo <tvanyo@aurora.tech>
2023-11-29 20:04:04 +0000

    commited out toggleterm and some clean up

--- lua/vanyo/remap.lua
+++ lua/vanyo/remap.lua
@@ -16,20 +16,20 @@ vim.keymap.set("n", "<leader>w", "<cmd>bp|bd #<CR>", { desc = "Close Buffer; Ret
 
 -- Moving between terminal and non-terminal windows
 -- Source: https://github.com/akinsho/toggleterm.nvim
-vim.keymap.set("n", "tt", ":ToggleTerm<CR>", { noremap = true, desc = "toggle terminal" })
-function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	-- Want to keep <esc> for vim mode in terminal, so need to use 'jk'
-	--vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-end
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+-- vim.keymap.set("n", "tt", ":ToggleTerm<CR>", { noremap = true, desc = "toggle terminal" })
+-- function _G.set_terminal_keymaps()
+-- 	local opts = { buffer = 0 }
+-- 	-- Want to keep <esc> for vim mode in terminal, so need to use 'jk'
+-- 	--vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+-- 	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+-- 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+-- 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+-- 	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+-- 	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+-- 	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+-- end
+-- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
 
 -- Yanking that makes sense to me
 vim.keymap.set("n", "Y", "yy", { noremap = true, desc = "yank entire line" })
@@ -56,7 +56,7 @@ vim.keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>", { noremap = true })
 -- vim.keymap.set("i", ":w", "<esc>:w<CR>", { noremap = true })
 
 -- search stuff
-vim.keymap.set("n", "<leader>/", ":noh<CR>", { noremap = true, desc = "turn off highlighting" })
+vim.keymap.set("n", "<leader>;", ":noh<CR>", { noremap = true, desc = "turn off highlighting" })
 
 -- open url
 vim.keymap.set(
