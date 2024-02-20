42d4e40984e271bc0d5d389bfa86e555928fc6ba
Todd Vanyo <tvanyo@aurora.tech>
2024-02-20 12:03:17 -0600

    commented out VeryLazy event

--- lua/vanyo/plugins/follow-md-links.lua
+++ lua/vanyo/plugins/follow-md-links.lua
@@ -1,4 +1,6 @@
 return {
 	"tvanyo/follow-md-links.nvim",
-	event = "VeryLazy",
+	--	Having VeryLazy set meant the full plugin wasn't being loaded when I opened a markdown
+	--	file from the command line. Why?
+	--	event = "VeryLazy",
 }
