61b37bd105c9e4a5dd39d87a6995670da66396e5
Todd Vanyo <tvanyo@aurora.tech>
2024-02-20 12:13:00 -0600

    changed loading to on markdown file

--- lua/vanyo/plugins/follow-md-links.lua
+++ lua/vanyo/plugins/follow-md-links.lua
@@ -3,4 +3,7 @@ return {
 	--	Having VeryLazy set meant the full plugin wasn't being loaded when I opened a markdown
 	--	file from the command line. Why?
 	--	event = "VeryLazy",
+	-- Setting the filetype keeps the plugin from loading until a markdown file is in the buffer.
+	-- True even if starting from command line.
+	ft = "markdown",
 }
