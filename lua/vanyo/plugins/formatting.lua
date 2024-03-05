05fa27d134fb44b05aa462815f3ce723a2e547fd
Todd Vanyo <tvanyo@aurora.tech>
2024-03-05 10:25:38 -0600

    moved keymaps to keymaps file to make conflict easier to deal with

--- lua/vanyo/plugins/formatting.lua
+++ lua/vanyo/plugins/formatting.lua
@@ -27,12 +27,12 @@ return {
 			},
 		})
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode) " })
+		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		-- 	conform.format({
+		-- 		lsp_fallback = true,
+		-- 		async = false,
+		-- 		timeout_ms = 500,
+		-- 	})
+		-- end, { desc = "Format file or range (in visual mode) " })
 	end,
 }
--- lua/vanyo/plugins/telescope.lua
+++ lua/vanyo/plugins/telescope.lua
@@ -16,7 +16,7 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local builtin = require("telescope.builtin")
+		--	local builtin = require("telescope.builtin")
 
 		telescope.setup({
 			defaults = {
@@ -32,9 +32,10 @@ return {
 		})
 
 		telescope.load_extension("fzf")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		telescope.load_extension("noice")
+		-- vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		-- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		-- vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 	end,
 }
