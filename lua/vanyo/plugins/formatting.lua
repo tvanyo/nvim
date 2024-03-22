baadafa7962f820ae971b24051557128480c90a6
Todd Vanyo <tvanyo@aurora.tech>
2024-03-22 15:24:47 -0500

    added prettier options to alleviate timeouts

--- lua/vanyo/plugins/formatting.lua
+++ lua/vanyo/plugins/formatting.lua
@@ -16,7 +16,7 @@ return {
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
-				graphql = { "prettier" },
+				-- graphql = { "prettier" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
@@ -26,6 +26,36 @@ return {
 				timeout_ms = 500,
 			},
 		})
+
+		-- Source: https://github.com/stevearc/conform.nvim/blob/master/doc/formatter_options.md#prettier
+		require("conform").formatters.prettier = {
+			options = {
+				-- Use a specific prettier parser for a filetype
+				-- Otherwise, prettier will try to infer the parser from the file name
+				ft_parsers = {
+					javascript = "babel",
+					--     javascriptreact = "babel",
+					typescript = "typescript",
+					--     typescriptreact = "typescript",
+					--     vue = "vue",
+					css = "css",
+					--     scss = "scss",
+					--     less = "less",
+					--     html = "html",
+					json = "json",
+					--     jsonc = "json",
+					yaml = "yaml",
+					markdown = "markdown",
+					--     ["markdown.mdx"] = "mdx",
+					--     graphql = "graphql",
+					--     handlebars = "glimmer",
+				},
+				-- Use a specific prettier parser for a file extension
+				ext_parsers = {
+					-- qmd = "markdown",
+				},
+			},
+		}
 
 		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 		-- 	conform.format({
