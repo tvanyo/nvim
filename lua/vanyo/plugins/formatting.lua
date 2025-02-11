e18f8c1c11b04e110c80c7435e8a89ee3f491abe
Todd Vanyo <tvanyo@aurora.tech>
2025-02-11 18:12:36 -0500

    trying to get consistent formating of markdown, may still need work

--- lua/vanyo/plugins/formatting.lua
+++ lua/vanyo/plugins/formatting.lua
@@ -2,15 +2,6 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre", "BufReadPre", "BufNewFile" },
 
-	-- source: https://github.com/fsouza/prettierd?tab=readme-ov-file
-	use_prettierd = function()
-		return {
-			exe = "prettierd",
-			args = { vim.api.nvim_buf_get_name(0) },
-			stdin = true,
-		}
-	end,
-
 	config = function()
 		local conform = require("conform")
 		conform.setup({
@@ -24,49 +15,55 @@ return {
 				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				markdown = { "prettier" },
-				-- markdown = { "use_prettierd" },
+				--markdown = { "prettier" },
+				markdown = { "markdownlint" },
 				-- graphql = { "prettier" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
+			formatters = {
+				prettier = {
+					append_args = { "--tab-width", "4" },
+				},
+				markdownlint = { args = { "MD007", "4" } },
+			},
 			format_on_save = {
 				lsp_fallback = true,
-				async = false,
+				--async = false,
 				timeout_ms = 2500,
 			},
 		})
 
 		-- Source: https://github.com/stevearc/conform.nvim/blob/master/doc/formatter_options.md#prettier
 		--require("conform").formatters.prettierd = {
-		require("conform").formatters.prettier = {
-			options = {
-				-- Use a specific prettier parser for a filetype
-				-- Otherwise, prettier will try to infer the parser from the file name
-				ft_parsers = {
-					javascript = "babel",
-					--     javascriptreact = "babel",
-					typescript = "typescript",
-					--     typescriptreact = "typescript",
-					--     vue = "vue",
-					css = "css",
-					--     scss = "scss",
-					--     less = "less",
-					--     html = "html",
-					json = "json",
-					--     jsonc = "json",
-					yaml = "yaml",
-					markdown = "markdown",
-					--     ["markdown.mdx"] = "mdx",
-					--     graphql = "graphql",
-					--     handlebars = "glimmer",
-				},
-				-- Use a specific prettier parser for a file extension
-				ext_parsers = {
-					-- qmd = "markdown",
-				},
-			},
-		}
+		-- require("conform").formatters.prettier = {
+		-- 	options = {
+		-- 		-- Use a specific prettier parser for a filetype
+		-- 		-- Otherwise, prettier will try to infer the parser from the file name
+		-- 		ft_parsers = {
+		-- 			javascript = "babel",
+		-- 			--     javascriptreact = "babel",
+		-- 			typescript = "typescript",
+		-- 			--     typescriptreact = "typescript",
+		-- 			--     vue = "vue",
+		-- 			css = "css",
+		-- 			--     scss = "scss",
+		-- 			--     less = "less",
+		-- 			--     html = "html",
+		-- 			json = "json",
+		-- 			--     jsonc = "json",
+		-- 			yaml = "yaml",
+		-- 			markdown = "markdown",
+		-- 			--     ["markdown.mdx"] = "mdx",
+		-- 			--     graphql = "graphql",
+		-- 			--     handlebars = "glimmer",
+		-- 		},
+		-- 		-- Use a specific prettier parser for a file extension
+		-- 		ext_parsers = {
+		-- 			-- qmd = "markdown",
+		-- 		},
+		-- 	},
+		-- }
 
 		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 		-- 	conform.format({
