ae7729224bf6ae5fc50d2be0e7fdf16d6622719c
Todd Vanyo <tvanyo@aurora.tech>
2025-02-16 11:26:28 -0500

    trying to make markdown work

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.markdown_recommended_style = 0
 
 require("vanyo.lazy")
 require("vanyo.core")
