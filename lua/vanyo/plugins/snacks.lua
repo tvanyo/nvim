971151baa81f21aa13bbac4fbcf802b202066792
Todd Vanyo <tvanyo@aurora.tech>
2025-02-20 13:51:47 -0500

    Cleanup
    Cleaned out layout options that had been commented out

--- lua/vanyo/plugins/snacks.lua
+++ lua/vanyo/plugins/snacks.lua
@@ -19,35 +19,6 @@ return {
 			ui_select = true,
 			reverse = true,
 			layout = { preset = "telescope" },
-			-- layouts = {
-			-- 	defaults = {
-			-- 		layout = {
-			-- 			box = "horizontal",
-			-- 			backdrop = true,
-			-- 			width = 0.8,
-			-- 			height = 0.9,
-			-- 			border = "none",
-			-- 			{
-			-- 				box = "vertical",
-			-- 				{ win = "list", title = " Results ", title_pos = "center", border = "rounded" },
-			-- 				{
-			-- 					win = "input",
-			-- 					height = 1,
-			-- 					border = "rounded",
-			-- 					title = "{title} {live} {flags}",
-			-- 					title_pos = "center",
-			-- 				},
-			-- 			},
-			-- 			{
-			-- 				win = "preview",
-			-- 				title = "{preview:Preview}",
-			-- 				width = 0.45,
-			-- 				border = "rounded",
-			-- 				title_pos = "center",
-			-- 			},
-			-- 		},
-			-- 	},
-			-- },
 		},
 		notifier = { enabled = true },
 		notify = { enabled = true },
