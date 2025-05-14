49a4d9d200274a3e4e91057911fe01d1844c905a
Todd Vanyo <tvanyo@aurora.tech>
2025-05-14 16:55:10 -0400

    Fixed warnings
    Added lua script boilerplate on variable definitions to eliminate the
    warnings

--- lua/vanyo/snippets/zsh.lua
+++ lua/vanyo/snippets/zsh.lua
@@ -1,20 +1,43 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
 return {
 	s(
-		{ trig = "#!", dscr = "Expand into base zsh script code" },
+		{ trig = "wkscrpt", dscr = "Expand into base zsh script code" },
 		fmta(
 			[=[
-#!/bin/zsh
-
-main() {
-	<>
-}
-
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]] || [[ "${ZSH_ARGZERO" == "$0" ]]; then
-	# Script is being executed directly
-	main "$@"
-fi
+			#!/bin/zsh
+			# Aurora Innovation, Inc. Proprietary and Confidential. Copyright <year>
+			
+			function main() {
+				<>
+			}
+			
+			if [[ "${BASH_SOURCE[0]}" == "${0}" ]] || [[ "$ZSH_ARGZERO" == "$0" ]]; then
+				# Script is being executed directly
+				main "$@"
+			fi
 			]=],
-			{ i(1, "# enter code here") }
+			{ year = p(os.date, "%Y"), i(1, "# enter code here") }
 		)
 	),
 }
