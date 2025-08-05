cd9c56a432dbb072717625bcff4b20f00fd16c60
Todd Vanyo <tvanyo@aurora.tech>
2025-08-05 16:45:29 -0400

    respaced with proper lua layout style

--- lua/vanyo/plugins/lsp/mason-lspconfig.lua
+++ lua/vanyo/plugins/lsp/mason-lspconfig.lua
@@ -1,49 +1,22 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {},
-	dependenices = {
-		{ "mason-org/mason.nvim", opt = {} },
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"pyright",
-				"pylsp",
-				"bashls",
-				"lua_ls",
-				"ts_ls",
-				"clangd",
-			},
-		})
-		vim.lsp.config("pyright", {
-			filetypes = { "python" },
-			settings = {
-				python = {
-					analysis = {
-						autoSearchPaths = true,
-						diagnosticMode = "openFilesOnly",
-						useLibraryCodeForTypes = true,
-					},
-				},
-			},
-		})
-		vim.lsp.config("lua_ls", {
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim", "require" },
-					},
-					workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-					telemetry = { enable = false },
-				},
-			},
-		})
-	end,
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependenices = {
+        { "mason-org/mason.nvim", opt = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "pyright",
+                "pylsp",
+                "bashls",
+                "lua_ls",
+                "ts_ls",
+                "clangd",
+            },
+        })
         vim.lsp.config("pylsp", {
             settings = {
                 pylsp = {
@@ -61,4 +34,30 @@ return {
             },
         })
         vim.lsp.config("pyright", {
+            filetypes = { "python" },
+            settings = {
+                python = {
+                    analysis = {
+                        autoSearchPaths = true,
+                        diagnosticMode = "openFilesOnly",
+                        useLibraryCodeForTypes = true,
+                    },
+                },
+            },
+        })
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        globals = { "vim", "require" },
+                    },
+                    workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+                    telemetry = { enable = false },
+                },
+            },
+        })
+    end,
 }
