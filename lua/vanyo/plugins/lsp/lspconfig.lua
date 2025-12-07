0db6d2ffadb71196df20836a57cb885c2817c6a4
Todd Vanyo <todd.vanyo@gmail.com>
2025-12-07 17:38:59 -0500

    reconfigured to simplify
    Using mason-lspconfig, which allows to use the names of lsps from
    nvim-lspconfig. No longer relying on my own configuration, but the
    group.

--- lua/vanyo/plugins/lsp/lspconfig.lua
+++
@@ -1,9 +0,0 @@
--- Original source https://github.com/josean-dev/dev-environment-files/blob/main/.config/nvim/lua/josean/plugins/lsp/lspconfig.lua
-return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		--"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-	},
-}
--- lua/vanyo/plugins/lsp/mason.lua
+++ lua/vanyo/plugins/lsp/mason.lua
@@ -1,55 +1,44 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
-		local mason_tool_installer = require("mason-tool-installer")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"shfmt",
-				"shellcheck",
-				"prettier",
-				"clangd",
-				"stylua",
-				"isort",
-				"black",
-				"pyright",
-				"bash-language-server",
-				"eslint_d",
-			},
-			auto_update = true,
-		})
-		vim.diagnostic.config({
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = " ",
-					[vim.diagnostic.severity.WARN] = " ",
-					[vim.diagnostic.severity.INFO] = " ",
-					[vim.diagnostic.severity.HINT] = "󰠠 ",
-				},
-				linehl = {
-					[vim.diagnostic.severity.ERROR] = "Error",
-					[vim.diagnostic.severity.WARN] = "Warn",
-					[vim.diagnostic.severity.INFO] = "Info",
-					[vim.diagnostic.severity.HINT] = "Hint",
-				},
-			},
-			jump = { float = true },
-		})
-	end,
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "eslint",
+      },
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opt = {
+      ensure_installed = {
+        "shfmt",
+        "shellcheck",
+        "prettier",
+        "clangd",
+        "stylua",
+        "isort",
+        "black",
+        "pyright",
+        "eslint_d",
+      },
+    },
+    dependencies = { "williamboman/mason.nvim" },
+  },
 }
