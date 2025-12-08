380fe88b1c06f20065cce0c3cb9926a68eaf841c
Todd Vanyo <tvanyo@aurora.tech>
2025-12-08 11:21:45 -0500

    added ts_ls for javascript lsp

--- lua/vanyo/plugins/lsp/mason.lua
+++ lua/vanyo/plugins/lsp/mason.lua
@@ -1,44 +1,45 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "eslint",
-      },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
+    {
+        "williamboman/mason-lspconfig.nvim",
         opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "eslint",
+                "ts_ls",
             },
-          },
         },
-      },
-      "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "williamboman/mason.nvim",
+                opts = {
+                    ui = {
+                        icons = {
+                            package_installed = "✓",
+                            package_pending = "➜",
+                            package_uninstalled = "✗",
+                        },
+                    },
+                },
+            },
+            "neovim/nvim-lspconfig",
+        },
     },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opt = {
-      ensure_installed = {
-        "shfmt",
-        "shellcheck",
-        "prettier",
-        "clangd",
-        "stylua",
-        "isort",
-        "black",
-        "pyright",
-        "eslint_d",
-      },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opt = {
+            ensure_installed = {
+                "shfmt",
+                "shellcheck",
+                "prettier",
+                "clangd",
+                "stylua",
+                "isort",
+                "black",
+                "pyright",
+                "eslint_d",
+            },
+        },
+        dependencies = { "williamboman/mason.nvim" },
     },
-    dependencies = { "williamboman/mason.nvim" },
-  },
 }
